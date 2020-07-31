c56e9b2e2976f88aae51be7c2406bf1d95685015
Aige <aigestudio@hotmail.com>
2020-07-31 17:28:36 +0800

    Initialize repository. 初始化仓库。

---
+++ Demo/src/main/java/com/aigestudio/datepicker/demo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.aigestudio.datepicker.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class MainActivity extends AppCompatActivity {
+    @Override
+    protected final void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
